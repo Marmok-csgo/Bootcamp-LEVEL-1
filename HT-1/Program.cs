dc4499e00dd8d0414a4bb0376a88b898ea9516d0
MrAke <aketostoboy@gmail.com>
2023-07-12 15:17:30 +0500

    Add project files.

---
+++ HT-1/Program.cs
@@ -0,0 +1,40 @@
+﻿//                  task-1
+//Console.WriteLine("Enter your name: ");
+//var name = Console.ReadLine();
+//Console.WriteLine("Enter your surname: ");
+//var surname = Console.ReadLine();
+//Console.WriteLine("Enter your age: ");
+//var age = Convert.ToInt32(Console.ReadLine());
+//Console.Write($"name:{name}\n" +
+//              $"surname:{surname}\n" +
+//              $"age:{age}");
+
+
+//                  task-2
+
+//DateTime dateTime = DateTime.Now;
+//var shortDateValue = dateTime.ToShortDateString();
+//var longDateValue = dateTime.ToLongDateString();
+//byte age = 75;
+//float money = 35.5F;
+//string name = "Max Developer";
+
+//Console.WriteLine($"{shortDateValue}\n" +
+//    $"{longDateValue}\n" +
+//    $"{age} yosh\n" +
+//    $"{money} $\n" +
+//    $"{name}");
+
+//                  task-3
+
+//var primitive1 = "DOTA2";
+//var complex1 = DateTime.Now;
+//Console.WriteLine($"{primitive1}\n" +
+//                  $"{complex1}");
+//Console.WriteLine("\n");
+//var valueBool = false;
+//var referens = "__MissionCompleted__";
+
+//Console.WriteLine($"{valueBool}\n" +
+//                  $"{referens}");
+
