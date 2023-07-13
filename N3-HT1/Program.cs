f5c963347000403762dc6728b319fd4ac6cfddd8
MrAke <aketostoboy@gmail.com>
2023-07-13 15:16:13 +0500

    hometaSk

---
+++ N3-HT1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Need numbers (true/false): ");
+        bool Numbers = Convert.ToBoolean(Console.ReadLine());
+
+        Console.Write("Need letters (true/false): ");
+        bool Letters = Convert.ToBoolean(Console.ReadLine());
+
+        Console.Write("Need symbols (true/false): ");
+        bool Symbols = Convert.ToBoolean(Console.ReadLine());
+
+        Console.Write("Password length: ");
+        int passwordLength = Convert.ToInt32(Console.ReadLine());
+
+        string password = GeneratePassword(Numbers, Letters, Symbols, passwordLength);
+        Console.WriteLine("Generated password: " + password);
+    }
+
+    static string GeneratePassword(bool includeNumbers, bool includeLetters, bool includeSymbols, int passwordLength)
+    {
+        const string numbers = "0123456789";
+        const string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string symbols = "!@#$%^&*()_-+=<>?";
+
+        Random random = new Random();
+
+        string characters = "";
+
+        if (includeNumbers)
+        {
+            characters += numbers;
+        }
+
+        if (includeLetters)
+        {
+            characters += letters;
+        }
+
+        if (includeSymbols)
+        {
+            characters += symbols;
+        }
+
+        if (characters.Length == 0)
+        {
+            throw new Exception("Choose one section!");
+        }
+
+        char[] passwordChars = new char[passwordLength];
+
+        for (int i = 0; i < passwordLength; i++)
+        {
+            int randomIndex = random.Next(characters.Length);
+            passwordChars[i] = characters[randomIndex];
+        }
+
+        return new string(passwordChars);
+    }
+}
---
+++ N3-HT2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter email: ");
+        string email = Console.ReadLine();
+
+        bool hasError = false;
+        bool hasUpperCase = false;
+
+        for (int i = 0; i < email.Length; i++)
+        {
+            char character = email[i];
+
+            if (!Char.IsLetterOrDigit(character) && character != '.' && character != '@')
+            {
+                Console.WriteLine("error symbol: " + character);
+                hasError = true;
+            }
+
+            if (Char.IsUpper(character))
+            {
+                hasUpperCase = true;
+            }
+        }
+
+        if (hasError)
+        {
+            Console.WriteLine("This incorrect email.");
+        }
+        else
+        {
+            if (hasUpperCase)
+            {
+                string lowercaseEmail = email.ToLower();
+                Console.WriteLine("Correct email: " + lowercaseEmail);
+            }
+            else
+            {
+                Console.WriteLine("Correct email: " + email);
+            }
+        }
+    }
+}
