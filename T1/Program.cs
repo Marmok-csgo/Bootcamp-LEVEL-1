3406cde1ece3eacd0cee7c2028736273ef58d6d7
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-07-14 14:19:56 +0500

    Delete T1 directory

--- T1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.Write("Name: ");
-var name = Console.ReadLine();
-Console.Write("Surname: ");
-var surname = Console.ReadLine();
-Console.Write("Age: ");
-var age = Console.ReadLine();
-Console.WriteLine("\"2+2*2\" misolini javobini toping!");
