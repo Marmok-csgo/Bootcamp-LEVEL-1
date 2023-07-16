947384108257feec5591fddf347711fd81cd71f8
MrAke <aketostoboy@gmail.com>
2023-07-16 19:02:26 +0500

    hh

---
+++ N5-HT1/Program.cs
@@ -0,0 +1,67 @@
+﻿string[] test = {
+    "What is alternative for else-if ?",
+    "Give 2 examples of the searching attribute.",
+    "What is the default value for boolean ?",
+    "what attribute we use to separate the string ?",
+    "What is 2+2*2 ?"
+    };
+
+string[] trueAnswers = {
+    "Switch case",
+    "indexof, contains",
+    "false",
+    "split",
+    "6"
+};
+
+string[] falseAnswers = {
+    "Switch Expression",
+    "Search, Find",
+    "true",
+    "join",
+    "8"
+};
+
+List<string> userAnswers = new List<string>();
+var score = 0;
+
+
+for (int i = 0; i < test.Length; i++)
+{
+    Console.Clear();
+    Console.WriteLine($"Question_{i + 1}:{test[i]}");
+    Console.WriteLine($"A){trueAnswers[i]}");
+    Console.WriteLine($"B){falseAnswers[i]}\n");
+    Console.Write("User:");
+    var user = Convert.ToString(Console.ReadLine()).ToUpper();
+    if ( user == "A")
+    {
+        score += 20;
+    }
+    else if ( user == "B")
+    {
+        userAnswers.Add(test[i]);
+    }
+    else
+    {
+        userAnswers.Add(test[i]);
+    }
+
+}
+
+Console.Clear();
+Console.WriteLine($"Score: {score}");
+if (userAnswers.Count == 0)
+{
+    Console.WriteLine("You are Genious!");
+}
+else {
+    Console.WriteLine("\t\t\t\t\tInvalid Answers:\n");
+    foreach (string answer in userAnswers)
+    {
+        int index = Array.IndexOf(test, answer);
+        Console.WriteLine($"Question_{index + 1}:{test[index]}\n" +
+            $"Answer:{trueAnswers[index]}");
+
+    }
+}
---
+++ N5-HT2/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Enter a number : ");
+var num1 = Convert.ToInt32(Console.ReadLine());
+while (true)
+{
+    Console.Write("Enter a command : ");
+    var command = Console.ReadLine();
+    
+    if(command == "+")
+    {
+        Console.Write("Enter a number : ");
+        var num2 = Convert.ToInt32(Console.ReadLine());
+        var Result = num1 + num2;
+        Console.WriteLine(Result);
+    }
+    if (command == "-")
+    {
+        Console.Write("Enter a number : ");
+        var num2 = Convert.ToInt32(Console.ReadLine());
+        var Result = num1 - num2;
+        Console.WriteLine(Result);
+    }
+    if (command == "/")
+    {
+        Console.Write("Enter a number : ");
+        var num2 = Convert.ToInt32(Console.ReadLine());
+        var Result = num1 / num2;
+        Console.WriteLine(Result);
+    }
+    if (command == "*")
+    {
+        Console.Write("Enter a number : ");
+        var num2 = Convert.ToInt32(Console.ReadLine());
+        var Result = num1 * num2;
+        Console.WriteLine($"Result : {Result}");
+    }
+    if (command == "exit" || command == "e")
+    {
+        Console.WriteLine("Thank you for using calculator");
+        break;
+    }
+
+}
