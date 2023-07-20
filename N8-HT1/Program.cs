862bac74d8c1d512c7556c420cb2384eeaa12333
MrAke <aketostoboy@gmail.com>
2023-07-20 15:46:50 +0500

    home

---
+++ N8-HT1/Program.cs
@@ -0,0 +1,45 @@
+﻿//                  List
+
+//Kirim Chiqimni hisoblab borish uchun.
+
+//Do'konda bor narsalarni saqlab qo'yish.
+
+//ismlar ro'yxati.
+
+
+
+//                  Stack
+
+
+// "Ctrl+Z" buning vazifasi oxirgi yozilgan matnni o'chirish.
+
+//Yig'ib qo'yilgan ko'rpalar. biz kurpa olish uchun oxirgi qo'yilganini olamiz.
+
+//klaviaturaning "Backspace" tugmasi oxirgi yozilgan matnni o'chiradi.
+
+
+//                  Queue
+
+//Chipta sotib olish navbati.
+
+//Buyurtmalar navbati.
+
+//o'yindagi jangda birinchi o'lgan birinchi tiriladi.
+
+
+//                  Linked List
+
+//kitoblar ro'yxati.
+
+//Toshkent shaharidagi mahallalar ro'yxati.
+
+//yoshlar royxati.
+
+
+//                  Dictionary
+
+//Shaxslar va ularning yoshlari.
+
+//Talabalar va ularning baholari.
+
+//Mahsulotlar va ularning narxlari.
