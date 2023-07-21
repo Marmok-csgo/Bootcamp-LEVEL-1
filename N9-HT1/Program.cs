5719c720ffdc8dc34b807f497c9d1e2f176e9126
MrAke <aketostoboy@gmail.com>
2023-07-21 16:04:00 +0500

    home-N9

---
+++ N9-HT1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+string[] emails = { 
+    "test@gmail.com",
+    "t.com",
+    "ake@gmail.com",
+    "ake__123@gmail.com",
+    "tele@@gmail.com"
+};
+
+string pattern = @"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$";
+var reGex = new Regex(pattern);
+
+foreach (string email in emails)
+{
+    if (reGex.IsMatch(email))
+    {
+        Console.WriteLine($"{email} - Valid.");
+    }
+    else
+    {
+        Console.WriteLine($"{email} - inValid."); 
+    }
+}
---
+++ N9-HT2/Program.cs
@@ -0,0 +1,64 @@
+﻿var events = new[]
+{
+    "O'qituvchi va murabbiylar Kuni",
+    "O'zbekiston Konstitutsiya Kuni",
+    "O'zbekiston Mustaqillik Kuni",
+    "Otalar kuni",
+    "Yoshlar Kuni",
+};
+
+var times = new[]
+{
+    new DateTime(2023, 9, 1, 12, 30, 0),
+    new DateTime(2023, 12, 8, 9, 0, 0),
+    new DateTime(2023, 10, 1, 10, 50, 0),
+    new DateTime(2023, 9, 11, 7, 30, 0),
+    new DateTime(2023, 12, 12, 16, 0, 0)
+};
+
+var counties = new[] { "en", "uz", "ru" };
+
+//Sorting
+for (var i = 0; i < events.Length; i++)
+{
+    for (var j = i + 1; j < events.Length; j++)
+    {
+        if (times[i] < times[j])
+        {
+            var temp = times[i];
+            times[i] = times[j];
+            times[j] = temp;
+
+            var temp2 = events[i];
+            events[i] = events[j];
+            events[j] = temp2;
+        }
+    }
+}
+
+foreach (var c in counties)
+{
+    Console.WriteLine(c);
+    if (c == "en")
+    {
+        for (var i = 0; i < events.Length; i++)
+        {
+            Console.WriteLine($" - {events[i]} - {times[i].ToString("dd.MM.yyyy hh:mm tt")}");
+        }
+    }
+    else if (c == "ru")
+    {
+        for (var i = 0; i < events.Length; i++)
+        {
+            Console.WriteLine($" - {events[i]} - {times[i].ToString("dd/MM/yyyy HH:mm")}");
+        }
+    }
+    else if (c == "uz")
+    {
+        for (var i = 0; i < events.Length; i++)
+        {
+            Console.WriteLine($" - {events[i]} - {times[i].ToString("dd.MM.yyyy HH:mm")}");
+        }
+    }
+
+}
