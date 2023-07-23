ec46871db21f841fd9f938c1689c06f3297fa241
MrAke <aketostoboy@gmail.com>
2023-07-23 11:58:59 +0500

    home-10-lesson

---
+++ N10-HT1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+}
+
+public class LibraryManagement
+{
+    private Dictionary<int, int> Books; 
+
+    public LibraryManagement()
+    {
+        Books = new Dictionary<int, int>();
+    }
+
+    public void AddBook(Book book, int numberOfCopies)
+    {
+        Books[book.Id] = numberOfCopies;
+    }
+
+    public bool Checkout(int bookId)
+    {
+        if (Books.ContainsKey(bookId))
+        {
+            if (Books[bookId] > 0)
+            {
+                Books[bookId]--; 
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book book1 = new Book { Id = 1, Title = "To Kill a Mockingbird", Author = "Harper Lee" };
+        Book book2 = new Book { Id = 2, Title = "1984", Author = "George Orwell" };
+        Book book3 = new Book { Id = 3, Title = "Pride and Prejudice", Author = "Jane Austen" };
+
+        LibraryManagement library = new LibraryManagement();
+        library.AddBook(book1, 3);
+        library.AddBook(book2, 5);
+        library.AddBook(book3, 2);
+
+        while (true)
+        {
+            Console.WriteLine("Available commands:");
+            Console.WriteLine("1 - Checkout a book");
+            Console.WriteLine("0 - Exit");
+
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                Console.WriteLine("Enter the book Id:");
+                int bookId = int.Parse(Console.ReadLine());
+
+                bool result = library.Checkout(bookId);
+                if (result)
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                }
+            }
+            else if (input == "0")
+            {
+                Console.WriteLine("Exiting the program...");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid command. Please try again.");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ N10-HT2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class ShoppingCart
+{
+    public List<Product> Items { get; set; }
+
+    public ShoppingCart()
+    {
+        Items = new List<Product>();
+    }
+
+    public void Add(Product product)
+    {
+        Product existingProduct = Items.Find(p => p.Id == product.Id);
+        if (existingProduct != null)
+        {
+            existingProduct.Name = product.Name;
+        }
+        else
+        {
+            Items.Add(product);
+        }
+    }
+
+    public bool Remove(Product product)
+    {
+        Product existingProduct = Items.Find(p => p.Id == product.Id);
+        if (existingProduct != null)
+        {
+            Items.Remove(existingProduct);
+            return true;
+        }
+
+        return false;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Product product1 = new Product { Id = 1, Name = "Ko'k ko'ylak" };
+        Product product2 = new Product { Id = 2, Name = "Qora palto" };
+        Product product3 = new Product { Id = 3, Name = "Oq yubka" };
+
+        ShoppingCart cart = new ShoppingCart();
+
+        cart.Add(product1);
+        cart.Add(product2);
+        cart.Add(product3);
+
+        Console.WriteLine("Korzinka mahsulotlari:");
+        foreach (var item in cart.Items)
+        {
+            Console.WriteLine($"Id: {item.Id}, Name: {item.Name}");
+        }
+
+        cart.Remove(product2);
+
+        Console.WriteLine("\nKorzinka holati mahsulot olindi:");
+        foreach (Product item in cart.Items)
+        {
+            Console.WriteLine($"Id: {item.Id}, Name: {item.Name}");
+        }
+    }
+}
