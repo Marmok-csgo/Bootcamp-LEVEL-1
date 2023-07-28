dbc82286694186e1fbc2494b8de30dfea782791d
MrAke <aketostoboy@gmail.com>
2023-07-28 16:18:01 +0500

    N14-hometask

---
+++ N14-HT1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.Contracts;
+var messager = new NotificationMessages();
+Console.WriteLine(messager.SearchMessage("AskPassword")); 
+Console.WriteLine(messager.SearchMessage("Tournament starts Tomorrow"));
+
+
+
+public class NotificationMessages
+{
+    private Dictionary<string, string> MessageDict = new Dictionary<string, string>();
+
+    public NotificationMessages()
+    {
+        MessageDict.Add("SuccRegistration", "You successfully registered");
+        MessageDict.Add("AskPassword", "Enter your password");
+        MessageDict.Add("Blocked", "Your account has been blocked");
+    }
+    protected KeyValuePair<string, string> FindMessage(string messageName)
+    {
+        foreach (var message in MessageDict)
+        {
+            if (message.Key.ToLower().Contains(messageName.ToLower()))
+                return message;
+        }
+        var notFound = new KeyValuePair<string, string>("NotFound", "Message topilmadi");
+        return notFound;
+    }
+
+    public string SearchMessage(string messageName)
+    {
+        return FindMessage(messageName).Value;
+    }
+}
---
+++ N14-HT2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using ClassLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var classRoom = new UltimateClassroomAttendance();
+        classRoom.Mark("Abdurahmon", true);
+        classRoom.Mark("Bunyod", true);
+        classRoom.Mark("Azizbek", false, "Meeting");
+        classRoom.Mark("Qurbonali", false, "Testing in Instite");
+
+        classRoom.Display();
+    }
+}
