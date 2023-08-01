b507c31086702a1309ef70f3fd1f599d96346160
MrAke <aketostoboy@gmail.com>
2023-08-01 12:15:15 +0500

    Equality

---
+++ Equality/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<string> group1 = new HashSet<string> { "John", "Tom", "Tim" };
+
+        HashSet<string> group2 = new HashSet<string> { "John", "Tim", "Tom" };
+        bool areEqual = group1.SetEquals(group2);
+
+        Console.WriteLine(areEqual); 
+    }
+}
