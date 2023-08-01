b794aaa611c9d72556b0e8921c4a4902b77850ce
MrAke <aketostoboy@gmail.com>
2023-08-01 12:13:54 +0500

    poject

---
+++ AllOne/Program.cs
@@ -0,0 +1,97 @@
+﻿using AllOne;
+using System.ComponentModel.Design;
+List<Student> student = new List<Student>()
+{
+    new Student(){ Id = 1, FullName = "Abdulxayev Jasurbek Anvar O'g'li",  BirthDay = new DateTime(2005, 12, 12),  EduType = "DotNET", Payment = 2400000, PaymentAmount = 2000000 },
+    new Student(){ Id = 2, FullName = "Abdumannonov Botirjon Davlat O'g'li",  BirthDay = new DateTime(2004, 1, 17),  EduType = "Python", Payment = 1600000, PaymentAmount = 500000 },
+    new Student(){ Id = 3, FullName = "Abdurahmonov Azizbek Alisher O'g'li",  BirthDay = new DateTime(2003, 12, 12),  EduType = "DotNET", Payment = 2400000, PaymentAmount = 3000000 },
+    new Student(){ Id = 4, FullName = "Anvarjonov Ozodbek Avazxon O'g'li",  BirthDay = new DateTime(2003, 11, 1),  EduType = "SMM", Payment = 1450000, PaymentAmount = 2500000 },
+    new Student(){ Id = 5, FullName = "Asadov Firdavs Asqarbek O'g'li",  BirthDay = new DateTime(2006, 7, 18),  EduType = "Dizayn", Payment = 1900000, PaymentAmount = 0 },
+    new Student(){ Id = 6, FullName = "Eshmurodov Umarali Abduhalim O'g'li",  BirthDay = new DateTime(2005, 12, 12),  EduType = "SMM", Payment = 1450000, PaymentAmount = 900000 },
+    new Student(){ Id = 7, FullName = "Jo'raboyev Boburjon Murodjon O'g'li",  BirthDay = new DateTime(2007, 4, 12),  EduType = "Dizayn", Payment = 1900000, PaymentAmount = 2000000 },
+    new Student(){ Id = 8, FullName = "Karimjonov Ilhom Ramzjon O'g'li",  BirthDay = new DateTime(2005, 8, 1),  EduType = "DotNET", Payment = 2400000, PaymentAmount = 2500000 },
+    new Student(){ Id = 9, FullName = "Komiljonov Muhammadaziz Abduraximovich",  BirthDay = new DateTime(2002, 1, 12),  EduType = "Python", Payment = 1600000, PaymentAmount = 1500000 },
+    new Student(){ Id = 10, FullName = "Rashidov Asadbek",  BirthDay = new DateTime(2001, 3, 12),  EduType = "Dizayn", Payment = 1900000, PaymentAmount = 1900000 },
+    new Student(){ Id = 11, FullName = "Sadriddinov Abdurahmon Jurabek O'g'li",  BirthDay = new DateTime(2003, 5, 12),  EduType = "Python", Payment = 1600000, PaymentAmount = 1700000 },
+    new Student(){ Id = 12, FullName = "Sattorova Habiba Shuxrat Qizi",  BirthDay = new DateTime(2006, 2, 7),  EduType = "DotNET", Payment = 2400000, PaymentAmount = 2000000 },
+    new Student(){ Id = 14, FullName = "Tolibov Firdavs Odil O'g'li",  BirthDay = new DateTime(2003, 5, 6),  EduType = "DotNET", Payment = 2400000, PaymentAmount = 2700000 },
+    new Student(){ Id = 15, FullName = "To'rayev Dinur Alisher O'g'li",  BirthDay = new DateTime(2002, 8, 22),  EduType = "Python", Payment = 1600000, PaymentAmount = 2000000 },
+    new Student(){ Id = 16, FullName = "Vositov Muhammadrizo Muhammadtolib O'g'li",  BirthDay = new DateTime(2000, 11, 20),  EduType = "SMM", Payment = 1450000, PaymentAmount = 1400000 },
+    new Student(){ Id = 17, FullName = "Xaybullayev Mexroj Mansurjonovich",  BirthDay = new DateTime(2005, 2, 12),  EduType = "Dizayn", Payment = 1900000, PaymentAmount = 2000000 },
+    new Student(){ Id = 18, FullName = "Xolmuratov Qurbonali Suxrob O'g'li",  BirthDay = new DateTime(2006, 5, 9),  EduType = "DotNET", Payment = 2400000, PaymentAmount = 2400000 },
+};
+
+int[,] balls = new int[,]
+{
+    { 90, 100, 75, 60 },
+    { 100, 100, 70, 0 },
+    { 90, 50, 60, 100 },
+    { 60, 50, 100, 90 },
+    { 100, 10, 75, 45 },
+    { 55, 85, 70, 60 },
+    { 95, 90, 40, 100 },
+    { 80, 100, 100, 40 },
+    { 65, 80, 100, 50 },
+    { 80, 90, 100, 60 },
+    { 90, 85, 65, 55 },
+    { 100, 55, 90, 100 },
+    { 80, 75, 90, 60 },
+    { 90, 90, 100, 55 },
+    { 100, 60, 90, 75 },
+    { 90, 80, 50, 100 },
+    { 100, 100, 60, 60 },
+    { 90, 50, 80, 90 },
+};
+
+var dotnet = "DotNET";
+var Dizayn = "Dizayn";
+var Python = "Python";
+var SMM = "SMM";
+
+
+while (true)
+{
+
+    Console.Write($"1 - List of students!\t 2 - Payment.\t 3 - Score of Students.\t 4 - Add Student.\t 5 - Remove Student.\nChoose one option: ");
+    char option = Console.ReadKey().KeyChar;
+
+    // section - 1
+    if (option.ToString() == "1")
+    {
+        Student.SectionOne(student);
+
+    }
+
+    // section - 2
+
+    if (option.ToString() == "2")
+    {
+        Student.SectionTwo(student);
+
+
+    }
+
+
+
+
+    // section - 3
+    if (option.ToString() == "3")
+    {
+        Student.SectionThree(student, balls);
+
+    }
+
+    //section - 4
+
+    if (option.ToString() == "4")
+    {
+        Student.AddStudent(student);
+
+    }
+
+    // section - 5
+    if (option.ToString() == "5")
+    {
+        Student.RemoveStudent(student);
+    }
+}
---
+++ AllOne/Student.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllOne
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public decimal Payment { get; set; }
+        public string EduType { get; set; }
+        public int PaymentAmount { get; set; }
+
+        public int getAge() => DateTime.Now.Year - BirthDay.Year;
+
+        public string shortName()
+        {
+            string[] substrings = FullName.Split();
+            return $"{substrings[0]} {substrings[1]}";
+        }
+         
+        public string GetAmount()
+        {
+            decimal amount = Payment - PaymentAmount;
+            if (amount > 0) return $"{amount} haqdor";
+            else return $"{-amount} qarzdor";
+        }
+
+        public static void AddStudent(List<Student> students)
+        {
+            Console.Clear();
+            Console.Write("FIO: ");
+            string FIO = Console.ReadLine();
+
+            Console.Write("Yunalish: ");
+            string EdutYpe = Console.ReadLine();
+
+            students.Add(new Student
+            {
+                Id = students.Last().Id + 1,
+                FullName = FIO,
+                BirthDay = DateTime.Now,
+                EduType = EdutYpe
+            });
+        }
+
+        public static void RemoveStudent(List<Student> students)
+        {
+            Console.Clear();
+            Console.Write("Enter index:");
+            int indexToRemove = Convert.ToInt32(Console.ReadLine());
+            students.RemoveAt(indexToRemove-1);
+        }
+
+
+        public static void SectionThree(List<Student> student, int[,] balls)
+        {
+            Console.Clear();
+            Console.WriteLine($"\tNames\t  |  10-may  | \t |  11-may  | \t |  12-may  | \t |  13-may  |\n");
+            for (int i = 0; i < balls.Length; i++)
+            {
+                Console.Write($"{student[i].shortName()}");
+
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(" \t" + balls[i, j] + "%\t");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
+        public static void SectionTwo(List<Student> student)
+        {
+            Console.Clear();
+            foreach (var item in student)
+            {
+                Console.WriteLine($"{item.shortName().PadRight(35, ' ')} {item.GetAmount()}");
+            }
+        }
+
+        public static void SectionOne(List<Student> student)
+        {
+                    Console.Clear();
+        Console.WriteLine($"1 - dotnet students\t" +
+            $"2 - Dizayn students\t" +
+            $"3 - Python students\t" +
+            $"4 - SMM students\t" +
+            $"0 - back");
+        Console.Write("Choose one class: ");
+        var classes = Console.ReadLine();
+
+        switch (classes)
+        {
+            case "1":
+                Console.Clear();
+                foreach (var item in student)
+                {
+                    if (item.EduType == "DotNET")
+                    {
+                        Console.WriteLine($"{item.Id} - {item.shortName()} - {item.getAge()} - {item.EduType}");
+
+                    }
+                }
+                break;
+            case "2":
+                Console.Clear();
+                foreach (var item in student)
+                {
+                    if (item.EduType == "Dizayn")
+                    {
+                        Console.WriteLine($"{item.Id} - {item.shortName()} - {item.getAge()} - {item.EduType}");
+
+                    }
+                }
+                break;
+            case "3":
+                Console.Clear();
+                foreach (var item in student)
+                {
+                    if (item.EduType == "Python")
+                    {
+                        Console.WriteLine($"{item.Id} - {item.shortName()} - {item.getAge()} - {item.EduType}");
+
+                    }
+                }
+                break;
+            case "4":
+                Console.Clear();
+                foreach (var item in student)
+                {
+                    if (item.EduType == "SMM")
+                    {
+                        Console.WriteLine($"{item.Id} - {item.shortName()} - {item.getAge()} - {item.EduType}");
+
+                    }
+                }
+                break;
+        }
+        }
+    }
+}
+
