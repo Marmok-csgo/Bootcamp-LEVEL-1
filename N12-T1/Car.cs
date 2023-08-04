e1779461409cb22a4ef3b21422e929ede4112b93
MrAke <aketostoboy@gmail.com>
2023-08-04 13:34:48 +0500

    hello there

---
+++ N12-T1/Car.cs
@@ -0,0 +1,27 @@
+﻿public class Car
+{
+    public string Model;   
+    public string Make;
+
+    public Car(string model, string make)
+    {
+        Model = model;
+        Make = make;
+    }
+
+    public override int GetHashCode()
+    {
+        int hash = 17;
+        hash = hash * 23 + Model.GetHashCode();
+        hash = hash * 23 + Make.GetHashCode();
+        return hash;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if(obj is Car car)
+            return car.GetHashCode() == obj.GetHashCode();
+
+        return false;
+    }
+}
---
+++ N12-T1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<Car> cars = new List<Car>
+        {
+            new Car("Accord", "Honda"),
+            new Car("Camry", "Toyota"),
+            new Car("Civic", "Honda"),
+            new Car("Accord", "Honda"),
+            new Car("Elantra", "Hyundai"),
+            new Car("Sonata", "Hyundai"),
+            new Car("Elantra", "Hyundai"),
+            new Car("Fusion", "Ford"),
+            new Car("Accord", "Honda"),
+            new Car("Malibu", "Chevrolet")
+        };
+
+        Dictionary<Car, int> carCount = new Dictionary<Car, int>();
+
+        foreach (Car car in cars)
+        {
+            if (carCount.ContainsKey(car))
+                carCount[car]++;
+            else
+                carCount.Add(car, 1);
+        }
+
+        foreach (var entry in carCount)
+        {
+
+            if(entry.Value > 1)
+            {
+                Console.WriteLine($"{entry.Key.Make} {entry.Key.Model}: {entry.Value}");
+
+            }
+        }
+    }
+}
