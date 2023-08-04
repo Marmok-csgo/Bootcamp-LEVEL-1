e4d2fb8b32ce404ab6e39c4076e5255437ef0eb9
MrAke <aketostoboy@gmail.com>
2023-08-04 13:35:27 +0500

    HI

---
+++ N14-T1/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N14_T1
+{
+    public class Animal
+    {
+
+
+        public virtual void FunFact()
+        {
+            Console.WriteLine("...");
+        }
+
+    }
+
+    public class Dalmatian : Animal
+    {
+        public override void FunFact()
+        {
+            Console.WriteLine("Dalmatians are born without spots!");
+        }
+
+    }
+
+
+    public class Cat : Animal
+    {
+        public override void FunFact()
+        {
+            Console.WriteLine("Cats have 32 muscles in each ear.");
+        }
+    }
+
+
+    public class Starfish : Animal
+    {
+        public override void FunFact()
+        {
+            Console.WriteLine("Starfish do not have a brain.");
+        }
+    }
+}
---
+++ N14-T1/Program.cs
@@ -0,0 +1,19 @@
+﻿using N14_T1;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Animal animal = new Animal();
+        animal.FunFact();
+
+        Animal Dog = new Dalmatian();
+        Dog.FunFact();
+
+        Animal Cat = new Cat();
+        Cat.FunFact();
+
+        Animal Fish = new Starfish();
+        Fish.FunFact();
+    }
+}
