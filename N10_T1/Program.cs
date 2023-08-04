fbb71bdbf33f80028b37ff8668235e7474ce157c
MrAke <aketostoboy@gmail.com>
2023-08-04 13:34:11 +0500

    hello

---
+++ N10_T1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Enter a first name: ");
+var firstName = Console.ReadLine();
+Console.Write("Enter a last name: ");
+var lastName = Console.ReadLine();
+Console.Write("Enter a age: ");
+var age = Convert.ToInt32(Console.ReadLine());
+
+var user = new User(firstName, lastName, age);
+
+Console.WriteLine($"{user.FirstName} - {user.LastName} - {user.Age}");
+
+class User
+{
+
+    public User(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+
+}
+
+
