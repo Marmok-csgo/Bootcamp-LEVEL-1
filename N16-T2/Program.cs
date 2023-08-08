256e2981b406ad7f0fab7a48c92a1758a98e1597
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-08 14:47:22 +0500

    task

---
+++ N16-T2/Program.cs
@@ -0,0 +1,18 @@
+﻿using N16_T2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var temp = new TemperatureSensorService();
+        temp.Temperature = "20";
+        temp.Temperature = "30";
+        temp.Temperature = "40";
+        temp.Temperature = "-10";
+        temp.Temperature = "-20";
+
+        temp.DisplayAll();
+
+
+    }
+}
---
+++ N16-T2/TemperatureSensorService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N16_T2
+{
+    internal class TemperatureSensorService
+    {
+        private List<string> TemperatureList = new List<string>();
+        private string _temperature;
+        public string Temperature
+        {
+            set
+            {
+                TemperatureList.Add(value);
+                _temperature = value;
+            }
+        }
+
+
+
+        public void DisplayAll()
+        {
+            if(TemperatureList.Count  == 0)
+            {
+                Console.WriteLine("List is Empty!");
+            }
+
+            foreach(string value in TemperatureList)
+            {
+                Console.WriteLine(value);
+            }
+
+
+        }
+    }
+}
