cd214f5c89de60fb7532cbb477c0979520130f52
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-08 14:49:19 +0500

    task

---
+++ N13-T3/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N13_T3
+{
+    public class BankAccount
+    {
+        public string FirstName;
+        public string LastName;
+        public double Deposit = -20;
+
+
+        public BankAccount(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+
+        public BankAccount(string firstname, string lastname, double deposit)
+            :this(firstname, lastname)
+        {
+            Deposit = deposit;
+            if (deposit > 0)
+            {
+                Deposit = deposit;
+            }
+            else
+            {
+                throw new Exception("Deposit must bigger than ZERO!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} - {LastName} - {Deposit}";
+        }
+    }
+}
---
+++ N13-T3/Program.cs
@@ -0,0 +1,15 @@
+﻿using N13_T3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount bankAccount1 = new BankAccount("Firdavs", "Tolibov");
+        BankAccount bankAccount2 = new BankAccount("ALi", "Turayev", -20);
+
+
+
+        Console.WriteLine(bankAccount1);
+        Console.WriteLine(bankAccount2);
+    }
+}
