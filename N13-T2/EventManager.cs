3553220c314c3962a6baf91e97452139a61b034c
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-08 14:49:36 +0500

    task

---
+++ N13-T2/EventManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N13_T2
+{
+    public class EventManager
+    {
+        Dictionary<string, DateOnly> EventDayDict = new Dictionary<string, DateOnly>();
+        Dictionary<string, TimeOnly> EventTimeDict = new Dictionary<string, TimeOnly>();
+
+        public void Add(string Datename, DateOnly dateOnly)
+        {
+            EventDayDict.Add(Datename, dateOnly);
+        }
+
+
+
+        public void Add(string TimeName, TimeOnly timeOnly)
+        {
+            EventTimeDict.Add(TimeName, timeOnly);
+        }
+
+        public void Display()
+        {
+            foreach (var item2 in EventTimeDict)
+            {
+                Console.WriteLine($"{item2.Key} - {item2.Value}");
+
+            }
+
+            foreach (var item1 in EventDayDict)
+            {
+                Console.WriteLine($"{item1.Key} - {item1.Value}");
+            }
+
+        }
+
+    }
+}
---
+++ N13-T2/Program.cs
@@ -0,0 +1,20 @@
+﻿using N13_T2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EventManager eventManager = new EventManager();
+        eventManager.Add("Gaming", new DateOnly(2023,7,27));
+        eventManager.Add("Meeting", new DateOnly(2023,7,28));
+        eventManager.Add("Tournament", new DateOnly(2023,7,29));
+
+        eventManager.Add("Breakfast", new TimeOnly(12, 30, 45));
+        eventManager.Add("Lunch", new TimeOnly(18, 30, 45));
+        eventManager.Add("SLeep", new TimeOnly(21, 00, 00));
+
+
+        eventManager.Display();
+
+    }
+}
