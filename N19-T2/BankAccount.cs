2ac3297e6e7b6bfde9bbc12e2eff10d75a17ec4a
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-08 14:29:40 +0500

    task

---
+++ N19-T2/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N19_T2
+{
+    abstract class BankAccount
+    {
+        protected string AccountHolderName { get; set; }
+        protected string AccountNumber { get; set; }
+        protected double Balance { get; set; }
+
+
+        public virtual void Depozit(double money)
+        {
+            if (money >= 1000)
+            {
+                Balance += money;
+            }
+            else
+            {
+                throw new Exception("You enter invalid money.");
+            }
+        }
+
+
+        public virtual void WithDraw(double money)
+        {
+            if (money < Balance)
+            {
+                Balance -= money;
+            }
+            else
+            {
+                throw new Exception("You enter invalid money.");
+            }
+        }
+    }
+}
