e33b2521c6c94688e914f6c16c8cecb5d5c2498d
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-08 14:30:45 +0500

    task

---
+++ N19-T1/Person.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N19_T1
+{
+    public class Person
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+        private string City { get; set; }
+
+        public Person()
+        {
+            Name = "John";
+            Age = 19;
+            City = "London";
+        }
+
+
+        public Person(string name, int age, string city)
+        {
+            Name=name;
+            Age=age;
+            City=city;
+        }
+
+        public Person(Person otherPerson)
+        {
+            Name=otherPerson.Name;
+            Age=otherPerson.Age;
+            City=otherPerson.City;
+        }
+
+
+        static Person()
+        {
+            Console.WriteLine("Static class called.");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, City: {City}");
+        }
+    }
+}
---
+++ N19-T1/Program.cs
@@ -0,0 +1,17 @@
+﻿using N19_T1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var person1 = new Person();
+        person1.Display();
+
+
+        var person2 = new Person("Alice", 25, "USA");
+        person2.Display();
+
+        var person3 = new Person(person2);
+        person3.Display();
+    }
+}
