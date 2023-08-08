712b7fe79bd15014fb13e0fce1c7fe52a00467d5
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-08 14:47:39 +0500

    task

---
+++ N16-T1/EmailService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N16_T1
+{
+    internal class EmailService
+    {
+        public EmailService(string credentialaddress, string credentialpassword, string name)
+        {
+            CredentialAddress = credentialaddress;
+            CredentialPassword = credentialpassword;
+            Name = name;
+        }
+        public string CredentialAddress { get; init; }
+        public string CredentialPassword { get; init; }
+        public string Name { get; init; }
+
+        public void SendEmail(string ToEmail, string name)
+        {
+            var senderEmail = "sultonbek.rakhimov.recovery@gmail.com";
+            var senderPassword = "szabguksrhwsbtie";
+
+            var mail = new MailMessage(senderEmail, ToEmail);
+            mail.Subject = "Siz muvaffaqiyatli registratsiyadan o'tdingiz";
+            mail.Body = $"Hurmatli {name}, siz sistemadan muvaffaqiyatli o'tdingiz";
+
+            var smtpClient = new SmtpClient("smtp.gmail.com", 587); // Replace with your SMTP server address and port
+            smtpClient.Credentials = new NetworkCredential(senderEmail, senderPassword);
+            smtpClient.EnableSsl = true;
+
+            smtpClient.Send(mail);
+        }
+    }
+}
---
+++ N16-T1/Program.cs
@@ -0,0 +1,20 @@
+﻿using N16_T1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Email:");
+        var email = Console.ReadLine();
+
+        Console.Write("Enter Password:");
+        var pass = Console.ReadLine();
+
+        Console.Write("Enter Name:");
+        var name = Console.ReadLine();
+
+        var emailServise = new EmailService(email, pass, name);
+
+        emailServise.SendEmail(email, name);
+    }
+}
