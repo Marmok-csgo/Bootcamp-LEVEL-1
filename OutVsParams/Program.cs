b5838207be35c63682148c04ab6c54c0893be7f3
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-09 10:14:38 +0500

    taask

---
+++ OutVsParams/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Array: ");
+        var array =  Console.ReadLine();
+
+        string[] numberArray = array.Split(' ');
+
+        int Max;
+        int Min;
+
+        FindMaxMin(numberArray, out Max, out Min);
+        Console.WriteLine($"Max: {Max}\n" +
+                          $"Min: {Min}");
+
+    }
+
+    static void FindMaxMin(string[] strs, out int max, out int min)
+    {
+        max = Convert.ToInt32(strs[0]);
+        for (int i = 1; i < strs.Length; i++)
+        {
+            if (Convert.ToInt32(strs[i]) > max)
+            {
+                max = Convert.ToInt32(strs[i]);
+            }
+        }
+
+        min = Convert.ToInt32(strs[0]);
+        for (int i = 1; i < strs.Length; i++)
+        {
+            if (Convert.ToInt32(strs[i]) < min)
+            {
+                min = Convert.ToInt32(strs[i]);
+            }
+        }
+
+    }
+}
