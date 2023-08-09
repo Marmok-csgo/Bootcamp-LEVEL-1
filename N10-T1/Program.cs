9c2b24b4b2be56f3d953a5ce31e62e8a2c00cc5d
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-09 10:15:59 +0500

    task

---
+++ N10-T1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write($"Enter a age: ");
+var age = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    if(age < 18)  
+    {
+        throw new ArgumentOutOfRangeException("Sorry, you are too young");
+    }
+    if(age > 90)
+    {
+        throw new ArgumentOutOfRangeException("Sorry, you are too old");
+    }
+    else
+    {
+        Console.WriteLine("Valid age");
+    }
+}
+catch (ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine(ex.Message);
+}
