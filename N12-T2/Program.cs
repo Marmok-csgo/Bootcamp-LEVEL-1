aac5cd0ce187973a30185280ceb86eb2cbf482a8
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-09 10:16:21 +0500

    task

---
+++ N12-T2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<User> queue = new Queue<User>();
+
+        queue.Enqueue(new User("Peter", "Michael", "Brown"));
+        queue.Enqueue(new User("John", "David", "Smith"));
+        queue.Enqueue(new User("Mary", "Anne", "Jones"));
+        queue.Enqueue(new User("G`ishtmat", "G`ishtmatov", "G`ishtmatovich"));
+
+        while (true)
+        {
+            Console.Write("Ismingizni kiriting: ");
+            string Name = Console.ReadLine();
+
+            Console.Write("Sharifingizni kiriting: ");
+            string Fname = Console.ReadLine();
+
+            Console.Write("Familyangizni kiriting: ");
+            string Surname = Console.ReadLine();
+
+
+            User currentUser = new User(Name, Fname, Surname);
+
+            if (queue.Contains(currentUser))
+            {
+                Console.WriteLine($"{Name}, uje navbatda sanku!");
+            }
+            else
+            {
+                queue.Enqueue(currentUser);
+                Console.WriteLine($"Navbatdagi foydalanuvchilar:");
+                foreach (User user in queue)
+                {
+                    Console.WriteLine($"{user.FirstName} {user.MiddleName} {user.LastName}");
+                }
+            }
+        }
+    }
+}
---
+++ N12-T2/User.cs
@@ -0,0 +1,31 @@
+﻿class User
+{
+    public string FirstName;
+    public string MiddleName;
+    public string LastName;
+
+    public User(string firstName, string middleName, string lastName)
+    {
+        FirstName = firstName;
+        MiddleName = middleName;
+        LastName = lastName;
+    }
+
+    public override int GetHashCode()
+    {
+        int hash = 17;
+        hash = hash * 23 + FirstName.GetHashCode();
+        hash = hash * 23 + MiddleName.GetHashCode();
+        hash = hash * 23 + LastName.GetHashCode();
+        return hash;
+    }
+
+    public override bool Equals(object obj)
+    {
+        User otherUser = (User)obj;
+        return FirstName == otherUser.FirstName &&
+               MiddleName == otherUser.MiddleName &&
+               LastName == otherUser.LastName;
+        return false;
+    }
+}
