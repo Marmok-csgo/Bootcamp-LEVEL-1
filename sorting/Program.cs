9d7c5aa3c2a48db50486f6521db2e74163b36185
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-09 10:17:15 +0500

    task

---
+++ sorting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> films = new List<string>
+        {
+            "Inception",
+            "Dark Knight",
+            "Top Gun 2",
+            "Don't Look Up",
+            "Top Gun"
+        };
+
+        Console.WriteLine("O'sish bo'yicha:");
+        films.Sort(); 
+        PrintList(films);
+
+        Console.WriteLine("\nKamayish bo'yicha:");
+        films.Reverse();
+        PrintList(films);
+    }
+
+    static void PrintList(List<string> list)
+    {
+        foreach (string item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
