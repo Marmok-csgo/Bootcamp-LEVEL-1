96c3052e13f0ff19a7599198b2c286a5df659204
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-22 10:54:24 +0500

    ????

---
+++ Q_1/Person.cs
@@ -0,0 +1,13 @@
+namespace Q_1;
+
+public class Person
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string? name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
---
+++ Q_1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Q_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>{1, 2, 3, 4, 5, 6, 7};
+        numbers.Add(4);
+        
+        
+        int sum = numbers.Sum();
+        Console.WriteLine($"Sum: {sum}");
+
+        List<double> squaredNumbers = numbers.Select(num => Math.Pow(num, 2)).ToList();
+        Console.WriteLine("Squared numbers: " + string.Join(", ", squaredNumbers));
+        
+        List<int> duplicateNumbers = new List<int>{1, 2, 3, 3, 4};
+        List<int> disticntNumbers = duplicateNumbers.Distinct().ToList();
+        
+        Console.WriteLine("Distinct numbers: " + string.Join(", ", disticntNumbers));
+        
+    }
+}
