05d8e67aa390ddec362f69042167ee20b2667636
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-08-23 13:41:25 +0500

    Home

---
+++ N29-HT1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+List<string> userNames = new List<string>{"MarmokCsgo", "AkeTostoboy", "NeverGiveUp", "Abdura24"};
+
+var createFileTasks = userNames.Select(user => Task.Run(() =>
+{
+    return File.Create($"{user.ToLower()}.docx");
+}));
+
+foreach (var task in createFileTasks)
+{
+    Console.WriteLine($"{task} uchun file yaratildi.");
+}
+
+var userFiles = await Task.WhenAll(createFileTasks);
